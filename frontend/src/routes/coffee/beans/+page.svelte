5839455e0083c907085ad20911f7e3dcc7a62e9f
Fattigman <jacob.karlstrom@gmail.com>
2025-07-05 15:14:35 +0200

    beans listed with region

--- frontend/src/routes/coffee/beans/+page.svelte
+++ frontend/src/routes/coffee/beans/+page.svelte
@@ -3,13 +3,13 @@
     import { Button } from "$lib/components/ui/button"
 
     async function getRecord() {
-        const resultList = await pb.collection('Beans').getFullList(1000, { expand: "countries"
+        const resultList = await pb.collection('Beans').getFullList(1000, { expand: "regions"
         });
         console.log(resultList);
         return resultList;
     }
 </script>
-<div class="card card-content">
+<div class="card card-content beans">
 {#if $currentUser}
         Here there will be beans
 
@@ -18,11 +18,17 @@
         {:then data}
             {#each data as bean}
             <br>
-                {bean.name}
-                {#each bean.expand.countries as country}
-                    {country.name}
-                {/each}
+                {bean.name}:
+                <br>
+                <ol>
+                    {#each bean.expand.regions as region}
+                        <li>
+                            <pre>   {region.country}, {region.province}</pre>
+                        </li>
+                    {/each}
+                </ol>
             {/each}
+            <br>
         {:catch error}
             <p style="color: red">{error.message}</p>
         {/await}
